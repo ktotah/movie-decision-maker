a1e9f672e8f65357ac9b96c9f54598a70b59db42
ktotah <ket2137@columbia.edu>
2024-01-29 22:25:27 -0500

    pseudocoded the storage.js file - including: marking movies as watched and using local storage, filtering watched movies, user preferences in local storage, and some pseudocode for event listeners (i.e., for 'watched' and 'mark as unwatched' buttons) which should ultimately go in the script.js file

--- src/js/utils/storage.js
+++ src/js/utils/storage.js
@@ -1,1 +1,109 @@
 // Functions for local storage operations
+
+////////////////////
+
+
+// Pseudocode for Marking Movies as Watched and Using Local Storage
+
+// Function to retrieve watched movies from local storage
+// function getWatchedMoviesFromLocalStorage():
+//      if local storage has 'watchedMovies' key:
+//          return list of watchedMovies from local storage
+//      else:
+//          return an empty list
+
+
+// Function to save watched movies to local storage
+// function saveWatchedMoviesToLocalStorage(watchedMovies):
+//      save 'watchedMovies' to local storage
+
+
+// Function to check if a specific movie has been watched
+// Function isMovieWatched(movieId):
+//      watchedMovies = getWatchedMoviesFromLocalStorage()
+//      return movieID is in watchedMovies
+
+
+// Function to toggle the watched status of a movie
+// function toggleWatchedStatus(movieId):
+//      watchedMovies = getWatchedMoviesFromLocalStorage()
+//      if movieId is in watchedMovies:
+//          remove movieId from watchedMovies // The movie is unmarked as watched
+//      else:
+//          add movieId to watchedMovies // The movie is marked as watched
+//      saveWatchedMoviesToLocalStorage(watchedMovies)
+
+
+// Function to update the UI based on watched status of movies
+// function updateUIForWatchedMovies():
+//      movieList = getAllMovies() // Assuming a function that fetches the current list of movies
+//      for each movie in the movieList:
+//          if  isMovieWatched(movie.id):
+//              update movie item in UI to show it's watched
+//              set 'mark as unwatched' button to visible
+//          else: 
+//              update movie item in UI to show it's NOT watched
+//              set 'watched' button to visible
+
+
+////////////////////
+
+
+// Pseudocode for Filtering Watched Movies
+
+// Function to filter out watched movies
+// function filterOutWatchedMovies(movieList):
+//      filteredList = []
+//      for movie in movieList:
+//          if not isMovieWatched(movie.id):
+//              add movie to filteredList
+//      return filteredList
+
+
+// Implementing the filter option in UI
+// When user toggles 'Hide Watched Movies':
+//      if toggle is on:
+//          currentMovieList = filterOutWatchedMovies(currentMovieList)
+//      else:
+//          currentMovieList = getAllMovies() // assuming a function to fetch all movies
+//      update movie list UI with currentMovieList
+
+
+////////////////////
+
+
+// Pseudocode for User Preferences in Local Storage
+
+// Function to save user preferences
+// function saveUserPreferences(preferences):
+//      save preferences to local storage with key 'userPreferences'
+
+// Function to get user preferences
+// function getUserPreferences():
+//      if local storage has 'userPreferences':
+//          return preferences from local storage
+//      else:
+//          return default preferences
+
+// On application load or user action 
+// When application loads or user updates preferences:
+//      preferences = getUserPreferences()
+//      applyPreferencesToUI(preferences)
+
+
+/////////////////////////
+// TO INCLUDE IN SCRIPT.JS:
+
+// Function for setting up eventlisteres
+    // function for event listener on 'watched' button
+    // function for event listener of 'mark as unwatched' button 
+
+// Function to initialize the applicatoin 
+// function initializeApp():
+//      displayMovies() // Render movies to the page
+//      updateUIForWatchedMovies() // Set the inital watched status for each movie
+
+// document.addEventListener('DOMContentLoaded'
+// Call initializeApp when the application loads to set up everything
+// initializeApp()
+
