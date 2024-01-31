933df346538879253cd2f7c583ab93fafd957e96
danielhe27 <jdah49d@gmail.com>
2024-01-30 21:03:25 -0500

    getting info from the apiurl

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@ const apiUrl = 'https://www.googleapis.com/youtube/v3/search';
 const apiUrl2 = 'http://www.omdbapi.com/';
 const apiKey2 = '402e4178';
 
+// Function to get movie info from OMDb
+function getMovieInfo(movieTitle) {
+  const omdbParams = {
+    apikey: apiKey2,
+    t: movieTitle
+  };
+}
