04efc7ffcf45e2b72a09686b1ebadc2c2c47183f
danielhe27 <jdah49d@gmail.com>
2024-01-30 20:53:06 -0500

    API Key & url

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const apiKey = 'AIzaSyDcOM0ne4VtAftRHqaOBMEWPGQ45UBT498';
+const apiUrl = 'https://www.googleapis.com/youtube/v3/search';
+const apiUrl2 = 'http://www.omdbapi.com/';
+const apiKey2 = '402e4178';
+
