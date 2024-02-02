340290497b8b99e2bfe25cc61c01b890d77790ac
danielhe27 <jdah49d@gmail.com>
2024-02-01 19:27:27 -0500

    function to call api and get info  omdb

--- src/js/api/omdb.js
+++ src/js/api/omdb.js
@@ -1,1 +1,11 @@
 // Functions to interact with the OMDB API
+const apiUrl2 = 'http://www.omdbapi.com/';
+const apiKey2 = '402e4178';
+
+// Function to get movie info from OMDb
+function getMovieInfo(movieTitle) {
+  const omdbParams = {
+    apikey: apiKey2,
+    t: movieTitle
+  };
+ 
