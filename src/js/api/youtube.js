82cf65814449ea5eaa08e20d9c9c459383e286d1
danielhe27 <jdah49d@gmail.com>
2024-02-01 19:57:56 -0500

    url build using query parameters

--- src/js/api/youtube.js
+++ src/js/api/youtube.js
@@ -13,3 +13,47 @@ function displayVideos(movieTitle) {
   };
 
 
+  // Construct the URL with query parameters
+  const youtubeUrl = new URL(apiUrl);
+  youtubeUrl.search = new URLSearchParams(youtubeParams);
+
+  fetch(youtubeUrl)
+    .then(response => response.json())
+    .then(data => {
+      const videosContainer = document.getElementById('videos-container');
+      videosContainer.innerHTML = ''; // Clear previous results
+
+      data.items.forEach(item => {
+        const videoTitle = item.snippet.title;
+        const videoId = item.id.videoId;
+
+        const videoElement = document.createElement('div');
+        videoElement.innerHTML = `
+          <h3>${videoTitle}</h3>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
+        `;
+
+        videosContainer.appendChild(videoElement);
+      });
+    })
+    .catch(error => console.error('Error fetching YouTube videos:', error));
+}
+
+// Function to get movie info from OMDb
+function getMovieInfo(movieTitle) {
+  const omdbParams = {
+    apikey: apiKey2,
+    t: movieTitle
+  };
+
+  // Construct the URL with query parameters
+  const omdbUrl = new URL(apiUrl2);
+  omdbUrl.search = new URLSearchParams(omdbParams);
+
+  fetch(omdbUrl)
+    .then(response => response.json())
+    .then(data => {
+      const movieInfoContainer = document.getElementById('movieInfo');
+      const trailerContainer = document.getElementById('trailer');
+
+  
