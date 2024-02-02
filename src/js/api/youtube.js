f210ca96cdafff1abb0232c31e406902980031bd
danielhe27 <jdah49d@gmail.com>
2024-02-01 19:54:38 -0500

    function to display videos on youtube

--- src/js/api/youtube.js
+++ src/js/api/youtube.js
@@ -1,3 +1,15 @@
 // api key  and url for youtube
 const apiKey = 'AIzaSyDcOM0ne4VtAftRHqaOBMEWPGQ45UBT498';
 const apiUrl = 'https://www.googleapis.com/youtube/v3/search';
+
+// Function to display YouTube videos
+function displayVideos(movieTitle) {
+  const youtubeParams = {
+    part: 'snippet',
+    q: movieTitle + ' trailer',
+    type: 'video',
+    maxResults: 5,
+    key: apiKey
+  };
+
+
