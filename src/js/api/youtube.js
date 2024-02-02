37b6b68fedead0396e7b4ecbefb9623b124d521d
danielhe27 <jdah49d@gmail.com>
2024-02-01 20:00:27 -0500

    parameters on youtube api

--- src/js/api/youtube.js
+++ src/js/api/youtube.js
@@ -56,4 +56,58 @@ function getMovieInfo(movieTitle) {
       const movieInfoContainer = document.getElementById('movieInfo');
       const trailerContainer = document.getElementById('trailer');
 
+    // Construct the URL with query parameters
+    const youtubeUrl = new URL(apiUrl);
+    youtubeUrl.search = new URLSearchParams(youtubeParams);
   
+    fetch(youtubeUrl)
+      .then(response => response.json())
+      .then(data => {
+        const videosContainer = document.getElementById('videos-container');
+        videosContainer.innerHTML = ''; // Clear previous results
+  
+        data.items.forEach(item => {
+          const videoTitle = item.snippet.title;
+          const videoId = item.id.videoId;
+  
+          const videoElement = document.createElement('div');
+          videoElement.innerHTML = `
+            <h3>${videoTitle}</h3>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
+          `;
+  
+          videosContainer.appendChild(videoElement);
+        });
+      })
+      .catch(error => console.error('Error fetching YouTube videos:', error));
+  }
+  
+  // Function to get movie info from OMDb
+  function getMovieInfo(movieTitle) {
+    const omdbParams = {
+      apikey: apiKey2,
+      t: movieTitle
+    };
+  
+    // Construct the URL with query parameters
+    const omdbUrl = new URL(apiUrl2);
+    omdbUrl.search = new URLSearchParams(omdbParams);
+  
+    fetch(omdbUrl)
+      .then(response => response.json())
+      .then(data => {
+        const movieInfoContainer = document.getElementById('movieInfo');
+        const trailerContainer = document.getElementById('trailer');
+  
+        if (data.Response === 'True') {
+          const movieTitle = data.Title;
+          const movieYear = data.Year;
+          const movieRating = data.imdbRating;
+          const trailerUrl = data.Trailer;
+
+  // this container it will include parameters that we need when we display our video
+          movieInfoContainer.innerHTML = `
+            <p>Title: ${movieTitle}</p>
+            <p>Year: ${movieYear}</p>
+            <p>IMDb Rating: ${movieRating}</p>
+          `;
